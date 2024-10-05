7e79a40488b58fedcfcab17ef1495b97bc620db4
thomassto35 <108702744+thomassto35@users.noreply.github.com>
2024-10-05 02:25:44 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//PUNTO NUMERO 1
+let nom = "Carlos";
+let ape = "Stoffel";
+let años = "21";
+alert("Bienvenido "+nom+" "+ape+" con "+años+ " años");
+
+//PUNTO NUMERO 2
+/*let nombre = prompt("Ingrese su Nombre: ", "");
+let apellido = prompt("Ingrese su Apellido: ", "");
+let edad = prompt("Ingrese su edad: ", "");
+alert("Bienvenido "+nombre+" "+apellido+" y tenes "+edad+" años.")
+*/
+//PUNTO NUMERO 3
+let nombre = prompt("Ingrese su Nombre: ", "");
+let apellido = prompt("Ingrese su Apellido: ", "");
+let edad = prompt("Ingrese su edad: ", "");
+
+let diasVivo = edad * 365
+
+alert("Bienvenido "+nombre+" "+apellido+" y tenes "+edad+" años y viviste "+diasVivo+" sin contar años biciestos")
